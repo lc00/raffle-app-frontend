2438bbff8f0337c58780b007e6bc701440fadad9
Leanne Cheung <leannecheung00@gmail.com>
2019-10-16 16:14:22 -0600

    started the app

---
+++ src/Tiers.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Tier(props) {
+  return (
+    <div>
+      <h2>Tiers:</h2>      
+      <h3>$1 = 1 ticket</h3>
+      <h3>$5 = 10 ticket</h3>
+      <h3>$10 = 25 ticket</h3>
+    </div>
+  )
+}
