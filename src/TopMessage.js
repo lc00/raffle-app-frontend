cc0feb5a9ff5d8f50e1647eb8b1af61ec5aca9a8
Leanne Cheung <leannecheung00@gmail.com>
2019-10-17 18:03:26 -0600

    clean up TopMessage code

--- src/TopMessage.js
+++ src/TopMessage.js
@@ -6,7 +6,7 @@ export default function TopMessage(props) {
   let emailValidated = false;
 
   function validateEmail(email) {
-    return props.info.email.match(/\w+@\w+.\w+/)
+    return props.info.email.match(/\w+@\w+[.]\w+/)
   }
 
   if (!props.info.name) arr.push("name");
@@ -21,15 +21,12 @@ export default function TopMessage(props) {
 
   switch (arr.length) {
     case 1:
-      newStr = "Please enter your " + arr.join() + ".";
+      newStr = `Please enter your ${arr.join()}.`;
       break;
     case 2:
-      newStr = "Please enter your " + arr.join(" and ") + ".";
+      newStr = `Please enter your ${arr.join(' and ')}.`;
       break;
     case 3:
-      // newArr.push(arr[0])
-      // newArr.push(arr[1])
-      // newArr.join(', ')
       newStr = `Please enter your ${arr[0]}, ${arr[1]}, and ${arr[2]}.`;
       break;
     case 0:
