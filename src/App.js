10b1132f110ba1713df14459857f25d355b4da05
Leanne Cheung <leannecheung00@gmail.com>
2019-10-27 16:33:43 -0600

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";  
 import "./App.css";
-import payloadSample from './payloadSample'
+// import payloadSample from './payloadSample'
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './Home'
