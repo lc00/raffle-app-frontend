813909a2bc632befb8689ed8b6cf21b8536e9768
Leanne Cheung <leannecheung00@gmail.com>
2019-10-29 18:26:05 -0600

    removed user mock data

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ class App extends React.Component {
     super()
 
     this.state ={
-      name: "meow",
-      email: "me@ow.com",
-      donationAmount: "1",
+      name: "",
+      email: "",
+      donationAmount: "",
       topMessage: "",
       tiers: [],
       prizes: [],
